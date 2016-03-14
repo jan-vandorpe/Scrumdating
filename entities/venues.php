fd656c23b21ed967dcef5e9fdff9a258188ad9c3
Wouter <wouter.vker@hotmail.be>
2016-03-14 15:24:54 +0100

    entiteit venues ingevuld

--- entities/venues.php
+++ entities/venues.php
@@ -1,8 +1,46 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class venues{
+    
+    private $venueID,$venueName,$venueCity,$venueStreet,$venueStreetNR,$venueCapacity;
+    
+    private static $venueNameMap = array();
 
+
+    public function __construct($venueID,$venueName,$venueCity,$venueStreet,$venueStreetNR,$venueCapacity) {
+       
+        if(!isset(self::$venueNameMap[$venueName])){
+        $this->venueID = $venueID;
+        $this->venueName = $venueName;
+        $this->venueCity = $venueCity;
+        $this->venueStreet = $venueStreet;
+        $this->venueStreetNR = $venueStreetNR;
+        $this->venueCapacity= $venueCapacity;
+        }
+    }
+    
+    public function getVenueID() {
+        return $this->venueID;
+    }
+    
+    public function getVenueName() {
+        return $this->venueName;
+    }
+    
+    public function getVenueCity() {
+        return $this->venueCity;
+    }
+    
+    public function getVenueStreet() {
+        return $this->venueStreet;
+    }
+    
+    public function getVenueStreetNR() {
+        return $this->venueStreetNR;
+    }
+    
+    public function getVenueCapacity() {
+        return $this->venueCapacity;
+    }
+    
+}
