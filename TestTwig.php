ad1e60f3371fedc427cc797696e29fe3a8438cfd
cyberMicha <michaswart@hotmail.com>
2016-03-14 15:00:34 +0100

    commit the shit

--- TestTwig.php
+++ TestTwig.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'Library\vendor\autoload.php';
 
-$loader = new Twig_Loader_Filesystem('Presentation');
+$loader = new Twig_Loader_Filesystem('presentation');
 $twig = new Twig_Environment($loader);
 $name="Jordy";
 $view = $twig->render('index.twig', array('name' => $name, 'username' => 'LoL'));
