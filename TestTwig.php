a1b1cf7d63d4e4d37388a20e5040b8e9a42197fb
gregoryReynaert <gregreynaert@yahoo.com>
2016-03-14 15:40:39 +0100

    Merge origin/master
    Conflicts:
    	TestTwig.php

--- TestTwig.php
+++ TestTwig.php
@@ -3,6 +3,6 @@ require_once 'Library\vendor\autoload.php';
 
 $loader = new Twig_Loader_Filesystem('Presentation');
 $twig = new Twig_Environment($loader);
-$name="Gregory";
-$view = $twig->render('index.twig', array('name' => $name, 'username' => 'Jordy is a noob'));
+$name="Jordy";
+$view = $twig->render('index.twig', array('name' => $name, 'username' => 'LoL'));
 print($view);
