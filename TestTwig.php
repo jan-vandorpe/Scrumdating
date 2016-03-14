3501494b41ebe7176f2d58777f709ae3b1c747fc
cyberMicha <michaswart@hotmail.com>
2016-03-14 14:55:26 +0100

    pull tree

---
+++ TestTwig.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Library\vendor\autoload.php';
+
+$loader = new Twig_Loader_Filesystem('Presentation');
+$twig = new Twig_Environment($loader);
+$name="Jordy";
+$view = $twig->render('index.twig', array('name' => $name, 'username' => 'LoL'));
+print($view);
