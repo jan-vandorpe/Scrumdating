20fd73f46332cfde19cdb84d9a23a198fdb4d95e
Jordy <jordyvanderstukken@hotmail.com>
2016-03-16 14:05:33 +0100

    index welcome message geust/user

--- data/UserDAO.php
+++ data/UserDAO.php
@@ -1,4 +1,5 @@
 <?php
+
 //data/UserDAO.php
 
 require_once 'DBConfig.php';
@@ -7,34 +8,31 @@ require_once 'entities/User.php';
 $test = new UserDAO();
 $test->getByID(3);
 
-class UserDAO{
-    
-    public function getAll(){
-        $sql ="SELECT * FROM users";
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING,  DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        $resultset = $dbh->query($sql);
-        $lijst = array();
-        foreach ($resultset as $rij) {
-            $user = new user($rij["userID"],$rij["username"],$rij["password"],$rij["email"],$rij["sex"],$rij["birthDate"],$rij["preference"],$rij["hairColor"],$rij["length"],$rij["build"],$rij["eyeColor"],$rij["oneNight"],$rij["longTerm"],$rij["friends"],$rij["bio"],$rij["region"],$rij["postcode"],$rij["occupation"],$rij["smoker"],$rij["admin"]);
-            array_push($lijst, $user);
-        }
-    $dbh = null;
-    return $lijst;
-    }
+class UserDAO {
 
-    public function getByID($userID) {       
-        $sql ="SELECT * FROM users WHERE userID = :ID";
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING,  DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute(array(':ID' => $userID));
-        $resultset = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $lijst = array();
-        
-        foreach ($resultset as $rij) {
-            $user = new user($rij["userID"],$rij["username"],$rij["password"],$rij["email"],$rij["sex"],$rij["birthDate"],$rij["preference"],$rij["hairColor"],$rij["length"],$rij["build"],$rij["eyeColor"],$rij["oneNight"],$rij["longTerm"],$rij["friends"],$rij["bio"],$rij["region"],$rij["postcode"],$rij["occupation"],$rij["smoker"],$rij["admin"]);
-            array_push($lijst, $user);
-        }
+  public function getAll() {
+    $sql = "SELECT * FROM users";
+    $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+    $resultset = $dbh->query($sql);
+    $lijst = array();
+    foreach ($resultset as $rij) {
+      $user = new user($rij["userID"], $rij["username"], $rij["password"], $rij["email"], $rij["sex"], $rij["birthDate"], $rij["preference"], $rij["hairColor"], $rij["length"], $rij["build"], $rij["eyeColor"], $rij["oneNight"], $rij["longTerm"], $rij["friends"], $rij["bio"], $rij["region"], $rij["postcode"], $rij["occupation"], $rij["smoker"], $rij["admin"]);
+      array_push($lijst, $user);
+    }
     $dbh = null;
     return $lijst;
-    }
+  }
+
+  public function getByID($userID) {
+    $sql = "SELECT * FROM users WHERE userID = :ID";
+    $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute(array(':ID' => $userID));
+    $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user = new user($rij["userID"], $rij["username"], $rij["password"], $rij["email"], $rij["sex"], $rij["birthDate"], $rij["preference"], $rij["hairColor"], $rij["length"], $rij["build"], $rij["eyeColor"], $rij["oneNight"], $rij["longTerm"], $rij["friends"], $rij["bio"], $rij["region"], $rij["postcode"], $rij["occupation"], $rij["smoker"], $rij["admin"]);
+
+    $dbh = null;
+    return $user;
+  }
+
 }
--- showIndex.php
+++ showIndex.php
@@ -13,13 +13,15 @@ $loader = new Twig_Loader_Filesystem('presentation');
 //laad nieuwe Twig Environment vanuit die map
 $twig = new Twig_Environment($loader);
 
-
 if(isset($_SESSION['login'])){ //Gebruiker al ingelogd?
   $userSvc = new UserService();
   $user = $userSvc->getUserById($_SESSION['login']); //haal gegevens van die gebruiker op
-  if ($user=null){ 
+  if ($user==null){ 
     unset($_SESSION['login']); //geen gebruiker gevonden? Unset de session want verkeerd
-  } 
+    $user = "Guest";
+  }
+} else {
+  $user = "Guest";
 }
 
 //zet alle variabelen klaar in de presentatie pagina
