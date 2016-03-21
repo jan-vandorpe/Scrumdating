61d6eb93e913b09309bbc82321c272508444ff7f
Jordy <jordyvanderstukken@hotmail.com>
2016-03-21 09:49:06 +0100

    eventTypeDetail + venueDetail added

--- adminControl.php
+++ adminControl.php
@@ -9,6 +9,20 @@ require_once 'service\VenueService.php';
 $loader = new Twig_Loader_Filesystem('presentation');
 $twig = new Twig_Environment($loader);
 
+//create new
+if (isset($_GET['new'])) {
+  //new event
+  if ($_GET['new'] = 'event') {
+    $eventSvc = new EventService();
+    $eventTypeList = $eventSvc->getEventTypeList();
+    //prepare twig page
+    $view = $twig->render('newEvent.twig',array('eventTypeList' => $eventTypeList));
+  }
+  //execute twig page
+  print($view);
+  exit(0);
+}
+
 //profile management
 if (isset($_GET['user'])) {
   $userSvc = new UserService();
@@ -16,38 +30,51 @@ if (isset($_GET['user'])) {
 
 //prepare twig page
   $view = $twig->render('profielDetail.twig', array('user' => $user));
+
+  //execute twig page
+  print($view);
+  exit(0);
 }
 
 //event management
-if(isset($_GET['event'])) {
+if (isset($_GET['event'])) {
   $eventSvc = new EventService();
   $event = $eventSvc->getEventByID($_GET['event']);
   $eventTypeList = $eventSvc->getEventTypeList();
-  
+
   $venueSvc = new VenueService();
   $venueList = $venueSvc->getVenueList();
 
   //prepare twig page
-  $view = $twig->render('eventDetail.twig', array('event' => $event,'eventTypeList' => $eventTypeList,'venueList'=>$venueList));
+  $view = $twig->render('eventDetail.twig', array('event' => $event, 'eventTypeList' => $eventTypeList, 'venueList' => $venueList));
+
+  //execute twig page
+  print($view);
+  exit(0);
 }
 
 //venue management
-if(isset($_GET['venue'])){
+if (isset($_GET['venue'])) {
   $venueSvc = new VenueService();
-  $venue=$venueSvc->getVenueByID($_GET['venue']);
-  
+  $venue = $venueSvc->getVenueByID($_GET['venue']);
+
   //prepare twig page
   $view = $twig->render('venueDetail.twig', array('venue' => $venue));
+
+  //execute twig page
+  print($view);
+  exit(0);
 }
 
 //event type management
-if(isset($_GET['eventtype'])){
+if (isset($_GET['eventtype'])) {
   $eventTypeSvc = new EventType();
   $eventType = $eventTypeSvc->getEventTypeByID();
-  
+
   //prepare twig page
   $view = $twig->render('eventTypeDetail.twig', array('eventType' => $eventType));
-}
 
-//execute twig page
-print($view);
+  //execute twig page
+  print($view);
+  exit(0);
+}
