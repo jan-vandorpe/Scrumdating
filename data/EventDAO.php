01ed66b0b3886f43e9d1b7530d55587a62a19807
Wouter <wouter.vker@hotmail.be>
2016-03-22 09:44:44 +0100

    lkj

--- data/EventDAO.php
+++ data/EventDAO.php
@@ -65,7 +65,16 @@ class EventDAO {
     $stmt = $dbh->prepare($sql);
     $stmt->execute(array(':ID'=> $evID));  
     $dbh = null;    
-  }
+  } 
+  
+  public function updateEvent($evntID,$evDate,$evName,$venID) {
+    
+    $sql="UPDATE events SET evntDate = :evDate, evntName = :evName, venueID = :venID WHERE evntID = :evntID";
+    $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute(array(':evDate'=> $evDate,':evName'=> $evName,':venID'=>$venID,':evntID'=>$evntID));  
+    $dbh = null;            
+  }  
   
   public function addEventType($evntName,$evntDescription,$evntPrice) {      
     $sql = "INSERT INTO eventtypes (evntName,evntDescription,evntPrice) values (:evntName, :evntDescription,:evntPrice)";
@@ -75,21 +84,10 @@ class EventDAO {
     $dbh = null;    
   }
   
-  public function updateEvent($evntID,$evDate,$evName,$venID) {
+  public function updateEventType($evntName,$evntDescription,$evntPrice) {
     
-    $sql="UPDATE events SET evntDate = :evDate, evntName = :evName, venueID = :venID WHERE evntID = :evntID";
-    $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute(array(':evDate'=> $evDate,':evName'=> $evName,':venID'=>$venID,':evntID'=>$evntID));  
-    $dbh = null;            
-  }
-  
-  public function updateEventType($evntName) {
-    $_POST["evntName"] = $evName;
-    $_POST["evntDescription"] = $evDescription; 
-    $_POST["evntPrice"] = $evPrice;  
     
-    $sql="UPDATE eventtypes SET evntName = :evName, evntDescription = :evDescription, evntPrice = :evPrice WHERE evntName = :evNameID";
+    $sql="UPDATE eventtypes SET evntName = :evntName, evntDescription = :evntDescription, evntPrice = :evntPrice WHERE evntName = :evNameID";
     $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
     $stmt = $dbh->prepare($sql);
     $stmt->execute(array(':evName'=> $evName,':evDescription'=> $evDescription,':evPrice'=>$evPrice,':evNameID'=>$evntName));  
@@ -102,5 +100,7 @@ class EventDAO {
     $stmt->execute(array(':name'=> $evntName));  
     $dbh = null;   
   }
+  
+  
   
 }
