3e16014fee5e4bbfe1fd27c48f94ec9cf667b775
Wouter <wouter.vker@hotmail.be>
2016-03-22 11:19:09 +0100

    login

--- data/UserDAO.php
+++ data/UserDAO.php
@@ -57,5 +57,22 @@ class UserDAO {
     $stmt->execute(array('userID'=>$userID,':username' => $username,':password'=>$password,':email'=>$email,':sex'=>$sex,':birthDate'=>$birthDate,':preference'=>$preference,':hairColor'=>$hairColor,':length'=>$length,':build'=>$build,':eyeColor'=>$eyeColor,':oneNight'=>$oneNight,':longTerm'=>$longTerm,':friends'=>$friends,':bio'=>$bio,':region'=>$region,':postcode'=>$postcode,':occupation'=>$occupation,':smoker'=>$smoker,':admin'=>$admin));  
     $dbh = null;    
   }
+  
+  public function checkLogin($username,$password) {
+    $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+    $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute(array(':username' => $username,':password' => $password));
+    $rij = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if(rij == null){
+        return $login = false;
+    }
+    else{
+        $user = new user($rij["userID"], $rij["username"], $rij["password"], $rij["email"], $rij["sex"], $rij["birthDate"], $rij["preference"], $rij["hairColor"], $rij["length"], $rij["build"], $rij["eyeColor"], $rij["oneNight"], $rij["longTerm"], $rij["friends"], $rij["bio"], $rij["region"], $rij["postcode"], $rij["occupation"], $rij["smoker"], $rij["admin"]);
+        return $user;
+    }
+    $dbh = null;
+  }
 
 }
