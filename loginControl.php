1ef1f8d414ab03696e20eeb7693ac3be26906168
Wouter <wouter.vker@hotmail.be>
2016-03-22 13:51:43 +0100

    registreer WERKT !!!!!

--- loginControl.php
+++ loginControl.php
@@ -50,6 +50,51 @@ if(isset($_POST['registreren'])){
     $email = $_POST['email'];
     
     $userSvc = new UserService();
-    $user = $userSvc->addUser($username, $password, $email, $sex, $birthDate, $preference, $hairColor, $length, $build, $eyeColor, $oneNight, $longTerm, $friends, $bio, $region, $postcode, $occupation, $smoker, $admin);
-    include_once 'newUser.twig';
+    $user = $userSvc->registreerUser($username, $password, $email);
+    $userID = $user->getUserID();
+    $view = $twig->render('newUser.twig',array('user'=>$user, 'target'=>"loginControl.php?userID=$userID"));
+    print($view);
+    exit(0);
+}
+
+if (isset($_POST["addUser"])) {
+  $userID = $_GET["userID"];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+  if(!isset($_POST['sex'])){$sex = "m";}else{$sex = $_POST['sex'];}
+  $birthDate = $_POST['birthDate'];
+  if(!isset($_POST['preference'])){$preference = "a";}else{$preference = $_POST['preference'];}
+  $hairColor = $_POST['hairColor'];
+  $length = $_POST['length'];
+  $build = $_POST['build'];
+  $eyeColor = $_POST['eyeColor'];
+  if (!isset($_POST['oneNight'])) {
+    $oneNight = 0;
+  }
+  else {
+    $oneNight = $_POST['oneNight'];
+  }
+  if (!isset($_POST['longTerm'])) {
+    $longTerm = 0;
+  }
+  else {
+    $longTerm = $_POST['longTerm'];
+  }
+  if (!isset($_POST['friends'])) {
+    $friends = 0;
+  }
+  else {
+    $friends = $_POST['friends'];
+  }
+  $bio = $_POST['bio'];
+  $region = $_POST['region'];
+  $postcode = $_POST['postcode'];
+  $occupation = $_POST['occupation'];  
+  if(!isset($_POST['smoker'])){$smoker = 0;}else{$smoker = $_POST['smoker'];}
+
+  $userSvc = new UserService();
+  $userSvc->updateUser($userID, $username, $password, $email, $sex, $birthDate, $preference, $hairColor, $length, $build, $eyeColor, $oneNight, $longTerm, $friends, $bio, $region, $postcode, $occupation, $smoker);
+  include_once 'showAllAttributes.php';
+  exit(0);
 }
