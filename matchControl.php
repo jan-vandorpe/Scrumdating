a6ae69145c1e7e1c4b4f89f93596729b7d34838b
Jordy <jordyvanderstukken@hotmail.com>
2016-03-23 15:24:31 +0100

    Matchen gemaakt

--- matchControl.php
+++ matchControl.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'library\vendor\autoload.php';
 require_once 'service\UserService.php';
 
@@ -15,7 +16,50 @@ if (isset($_SESSION["login"])) {
 }
 else {
   $login = false;
+  //zet alle variabelen klaar in de presentatie pagina
+  $view = $twig->render('index.twig', array('login' => $login));
+
+//toon de pagina
+  print($view);
+  exit(0);
 }
+$userSex = $login->getSex();
+$userPreference = $login->getPreference();
+
+$userSvc = new UserService();
+
+if($userSex == 'm'){
+  if($userPreference == 'm'){
+    $matchSex = 'm';
+    $matchPreference = 'm';
+  }
+  if($userPreference == 'f'){
+    $matchSex = 'f';
+    $matchPreference = 'm';
+  }
+  if($userPreference == 'a'){
+    $matchSex = null;
+    $matchPreference = 'a';
+  }
+} else {
+  if($userPreference == 'm'){
+    $matchSex = 'm';
+    $matchPreference = 'f';
+  }
+  if($userPreference == 'f'){
+    $matchSex = 'f';
+    $matchPreference = 'f';
+  }
+  if($userPreference == 'a'){
+    $matchSex = null;
+    $matchPreference = 'a';
+  }
+}
+
+$matches=$userSvc->getUsersBySexuality($matchSex, $matchPreference);
+
+$view = $twig->render('matchen.twig',array('matches'=>$matches,'login'=>$login));
+print($view);
 
 
 
